c88a03e9b112855b3e7876930a42d854423d403a
Lyndon Panton <LyndonPanton@gmail.com>
2019-03-26 23:02:06 +0000

    Added checks for erroneous input
    fail if the input is empty, not a number, not an integer or  not more than zero

--- js/app.js
+++ js/app.js
@@ -5,4 +5,23 @@ yearSpan.textContent = year;
 const form = document.getElementById("collatz-form");
 form.addEventListener("submit", function(e) {
 	e.preventDefault();
+
+	let number = Number(document.getElementById("colllatz-form-input").value);
+	let count = 0;
+
+	if (number == "") { // If no value was entered...
+		// Make output area changes
+		console.log("Enter a value.");
+	} else if (typeof(number) !== "number") { // If the value enter is not a number...
+		// Make output area changes
+		console.log("Enter a numerical value.");
+	} else if (number % 1 !== 0) { // If the value entered is not an integer...
+		// Make output area changes
+		console.log("Enter an integral value.");
+	} else if (number <= 0) { // If the value enter is less or equal to 0...
+		// Make output area changes
+		console.log("Enter a non-zero positive value.");
+	} else { // If the value is valid...
+
+	}
 });
