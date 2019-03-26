e95936c7c02aa221f0e060c3cd1ffe20a027e1a5
Lyndon Panton <LyndonPanton@gmail.com>
2019-03-26 22:54:17 +0000

    Added submit event listener to the form
    First the event listener prevents the page for refreshing when submitted

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 const year = (new Date()).getFullYear();
 const yearSpan = document.getElementById("current-year");
 yearSpan.textContent = year;
+
+const form = document.getElementById("collatz-form");
+form.addEventListener("submit", function(e) {
+	e.preventDefault();
+});
