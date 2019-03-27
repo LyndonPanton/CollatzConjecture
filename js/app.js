46dabbff91103f3d371e0bcdbbf948d9699ce4a6
Lyndon Panton <LyndonPanton@gmail.com>
2019-03-27 23:19:14 +0000

    Restarted collatz conjecture logic

--- js/app.js
+++ js/app.js
@@ -9,36 +9,69 @@ form.addEventListener("submit", function(e) {
 	let number = Number(document.getElementById("collatz-form-input").value);
 	let count = 0;
 
-	if (number == "") { // If no value was entered...
-		// Make output area changes
-		console.log("Enter a value.");
-	} else if (typeof(number) !== "number") { // If the value enter is not a number...
-		// Make output area changes
-		console.log("Enter a numerical value.");
-	} else if (number % 1 !== 0) { // If the value entered is not an integer...
-		// Make output area changes
-		console.log("Enter an integral value.");
-	} else if (number <= 0) { // If the value enter is less or equal to 0...
-		// Make output area changes
-		console.log("Enter a non-zero positive value.");
-	} else { // If the value is valid...
-		const calculation = document.getElementById("collatz-output-calculation");
-		let line = document.createElement("p");
-		line.id = "line";
-		// console.log(number);
-		while (number != 1) {
+	// if (number == "") { // If no value was entered...
+	// 	// Make output area changes
+	// 	console.log("Enter a value.");
+	// } else if (typeof(number) !== "number") { // If the value enter is not a number...
+	// 	// Make output area changes
+	// 	console.log("Enter a numerical value.");
+	// } else if (number % 1 !== 0) { // If the value entered is not an integer...
+	// 	// Make output area changes
+	// 	console.log("Enter an integral value.");
+	// } else if (number <= 0) { // If the value enter is less or equal to 0...
+	// 	// Make output area changes
+	// 	console.log("Enter a non-zero positive value.");
+	// } else { // If the value is valid...
+	// 	const calculation = document.getElementById("collatz-output-calculation");
+	// 	let line = document.createElement("p");
+	// 	line.id = "line";
+	// 	// console.log(number);
+	// 	while (number != 1) {
+	// 		++count;
+	// 		calculation.textContent = "";
+	// 		let previous = number;
+	// 		if (number % 2 === 0) {
+	// 			number = number / 2;
+	// 			console.log(`${count}: ${previous} / 2 = ${number}`);
+	// 		} else {
+	// 			number = (number * 3) + 1;
+	// 			console.log(`${count}: ${previous} x 3 + 1 = ${number}`);
+	// 		}
+	// 	}
+
+	// 	console.log(`It took ${count} runs to get to 1`);
+	// }
+
+	let area = document.getElementById("collatz-output-calculation");
+
+	if (number == "") { // If no value was entered..
+		console.log("Enter a value that is not 0.");
+		area.textContent = "Enter a value";
+	} else if (typeof(number) !== "number") { // If the value enter was not a number..
+		console.log("Enter a numerical value");
+		area.textContent = "Enter a numerical value";
+	}  else if (number <= 0) { // If the value entered was not positive..
+		console.log("Enter a non-zero positive value");
+		area.textContent = "Enter a non-zero positive value";
+	} else if (number % 1 == 0) { // If the value entered was not an integer..
+		console.log("Enter an integral value");
+		area.textContent = "Enter an integral value";
+	} else { // If the value entered was valid..
+		console.log("Valid value entered");
+
+		while (number !== 1) {
 			++count;
-			calculation.textContent = "";
+
 			let previous = number;
 			if (number % 2 === 0) {
 				number = number / 2;
 				console.log(`${count}: ${previous} / 2 = ${number}`);
 			} else {
 				number = (number * 3) + 1;
-				console.log(`${count}: ${previous} x 3 + 1 = ${number}`);
+				console.log(`${count}: (${previous} * 3) + 1 = ${number}`);
 			}
 		}
 
-		console.log(`It took ${count} runs to get to 1`);
+		console.log(`It took ${count} turns to get to 1`);
 	}
 });
